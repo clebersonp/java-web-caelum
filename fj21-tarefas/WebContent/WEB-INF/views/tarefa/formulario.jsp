d97eff1c7211790a155d28971f4c104cf76f28a3
Cleberson <pauluci.cleberson@gmail.com>
2018-12-11 22:21:01 -0200

    adicionadas libs do hibernate validator e especificacao bean validation e criado validacoes no modelo tarefa

--- fj21-tarefas/WebContent/WEB-INF/views/tarefa/formulario.jsp
+++ fj21-tarefas/WebContent/WEB-INF/views/tarefa/formulario.jsp
@@ -1,5 +1,8 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+    
+<%@ taglib uri="http://www.springframework.org/tags/form" prefix="form" %> 
+    
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -9,6 +12,13 @@
 <body>
 	<form action="adicionaTarefa" method="post">
 		Descrição: <br/>
+		
+		<!--
+		taglib do spring para recuperar a msg de erro do BindingResult do controller.
+		Recupero o erro de um campo especifico do obj tarefa.
+	-->
+	<form:errors path="tarefa.descricao" cssStyle="color: red;" /> <br />
+		
 		<textarea rows="5" cols="100" name="descricao"></textarea><br/>
 		<input type="submit" value="Adicionar">
 	</form>
--- fj21-tarefas/src/br/com/caelum/tarefas/controller/TarefasController.java
+++ fj21-tarefas/src/br/com/caelum/tarefas/controller/TarefasController.java
@@ -1,8 +1,10 @@
 package br.com.caelum.tarefas.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 import br.com.caelum.tarefas.dao.JdbcTarefaDao;
 import br.com.caelum.tarefas.modelo.Tarefa;
@@ -16,19 +18,26 @@ public class TarefasController {
 		return "tarefa/formulario"; // mapeamento da jsp
 	}
 	
-	/*@RequestMapping("adicionaTarefa")
-	public String adiciona(final Tarefa tarefa) { // o spring se vira para fazer o binding, o campo do form tem que ser o mesmo do dominio
+	// o @Valid é para falar ao spring para passar para o hibernate validator fazer as validacoes
+	// o BindingResult é para o spring mvc não lancar a exception e eu conseguir captura-la
+	@RequestMapping("adicionaTarefa")
+	public String adiciona(final @Valid Tarefa tarefa, BindingResult result) { // o spring se vira para fazer o binding, o campo do form tem que ser o mesmo do dominio
+		
+		if (result.hasFieldErrors("descricao")) { // campo do obj tarefa.descricao
+			return "tarefa/formulario"; // continua na mesma pagina
+		}
+		
 		final JdbcTarefaDao dao = new JdbcTarefaDao();
 		dao.adiciona(tarefa);
 		return "tarefa/adicionada";
-	}*/
+	}
 	
-	@RequestMapping("adicionaTarefa")
+	/*@RequestMapping("adicionaTarefa")
 	public ModelAndView adiciona(final Tarefa tarefa) { // o spring se vira para fazer o binding, o campo do form tem que ser o mesmo do dominio
 		final JdbcTarefaDao dao = new JdbcTarefaDao();
 		dao.adiciona(tarefa);
 		final ModelAndView model = new ModelAndView("tarefa/adicionada");
 		model.addObject("tarefa", tarefa.getDescricao());
 		return model;
-	}
+	}*/
 }
